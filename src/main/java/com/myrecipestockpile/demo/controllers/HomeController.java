3c3cf9c4be5b968d9ef1414fb017f6e6a28bec61
abbyperez <abbyperez28@gmail.com>
2018-01-29 17:09:04 -0600

    User view model is functioning and carries object into post controller.

--- src/main/java/com/myrecipestockpile/demo/controllers/HomeController.java
+++ src/main/java/com/myrecipestockpile/demo/controllers/HomeController.java
@@ -1,7 +1,11 @@
 package com.myrecipestockpile.demo.controllers;
 
+import com.myrecipestockpile.demo.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,6 +15,18 @@ public class HomeController {
 //    @ResponseBody
     public String index() {
         return "index";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpUser( @ModelAttribute User user){
+        return "login";
+    }
+
+    @GetMapping("/sign-up")
+    public String showSignUpForm( Model vModel){
+        vModel.addAttribute("user", new User());
+        return "signup";
+
     }
 }
 
