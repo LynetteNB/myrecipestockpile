e7ab15827189deec3955587233274b7ddcc9e2e8
stephencisler <stephencisler@gmail.com>
2018-01-29 14:30:27 -0600

    Created Instruction model and updaded both for relationships to User and Stockpile Models

---
+++ src/main/java/com/myrecipestockpile/demo/Models/Instruction.java
@@ -0,0 +1,53 @@
+package com.myrecipestockpile.demo.Models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "recipe_instructions")
+public class Instruction {
+
+    @Id @GeneratedValue
+    private long id;
+
+    @Column(nullable = false)
+    private String instruction;
+
+    // Connects to recipes table, it represents the recipe id which this instruction belongs to.
+    @ManyToOne
+    private Recipe recipe;
+
+    // ------------------------------------------
+    // Constructors
+    // ------------------------------------------
+
+    public Instruction() {
+    }
+
+    // ------------------------------------------
+    // Getters / Setters
+    // ------------------------------------------
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getInstruction() {
+        return instruction;
+    }
+
+    public void setInstruction(String instruction) {
+        this.instruction = instruction;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
--- src/main/java/com/myrecipestockpile/demo/Models/Recipe.java
+++ src/main/java/com/myrecipestockpile/demo/Models/Recipe.java
@@ -2,6 +2,7 @@ package com.myrecipestockpile.demo.Models;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "recipes")
@@ -34,4 +35,108 @@ public class Recipe {
     // Privacy is set to false by default
     @Column(nullable = false)
     private boolean privateRecipe = false;
+
+    // Connects to users table, it represents it's owner.
+    @ManyToOne
+    private User user;
+
+    // Connects to another recipe's id, it represents the recipe which this one is a variation of. If null, it has no parent recipe and is not a variation.
+    @OneToOne
+    private Recipe parentRecipe;
+
+    // Connects to instructions table, to show which instructions belong to it. Instructions sorted by primary key will show proper order.
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private List<Instruction> instructions;
+
+
+    // Connects to stockpile table. The pivot table formed shows recipe/stockpile
+    @ManyToMany(mappedBy = "stockpileRecipes")
+    private List<Stockpile> stockpiles;
+
+    // Connects to recipes table. The pivot table formed shows user/recipe combinations to show which users have "hearted" which recipes.
+    @ManyToMany(mappedBy = "heartedRecipes")
+    private List<User> heartedUsers;
+
+
+    // Method to insert current date on insert into database
+    @PrePersist
+    protected void onCreate() {
+        dateCreated = new Date();
+    }
+
+    // ------------------------------------------
+    // Constructors
+    // ------------------------------------------
+
+    public Recipe() {
+    }
+
+    // ------------------------------------------
+    // Getters / Setters
+    // ------------------------------------------
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(int cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public int getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(int prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public void setServings(int servings) {
+        this.servings = servings;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public boolean isPrivateRecipe() {
+        return privateRecipe;
+    }
+
+    public void setPrivateRecipe(boolean privateRecipe) {
+        this.privateRecipe = privateRecipe;
+    }
 }
