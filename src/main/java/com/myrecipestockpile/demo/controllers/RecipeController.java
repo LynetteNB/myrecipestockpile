c682481ee1c0d1df82e65160a3755982d4da2b3a
abbyperez <abbyperez28@gmail.com>
2018-01-30 11:40:26 -0600

    Inserted empty @getmapping for index and showRecipe and created recipe directory to include html files.

--- src/main/java/com/myrecipestockpile/demo/controllers/RecipeController.java
+++ src/main/java/com/myrecipestockpile/demo/controllers/RecipeController.java
@@ -1,11 +1,25 @@
 package com.myrecipestockpile.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class RecipeController {
 
     // Inject dependency when Repository is ready
+    @GetMapping("/recipe/search")
+//    @ResponseBody
+    public String index(){
+        return "recipe/index";
+    }
+
+    @GetMapping("/recipe/showRecipe")
+//    @ResponseBody
+    public String recipe(){
+        return "recipe/showRecipe";
+    }
+
 
 
 }
