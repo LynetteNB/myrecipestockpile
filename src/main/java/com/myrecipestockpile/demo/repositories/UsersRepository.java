4090d75eb82a94f94ad109a4f406e76d07315c79
abbyperez <abbyperez28@gmail.com>
2018-01-30 10:20:20 -0600

    Recipe and Users Repositories

---
+++ src/main/java/com/myrecipestockpile/demo/repositories/UsersRepository.java
@@ -0,0 +1,9 @@
+package com.myrecipestockpile.demo.repositories;
+
+import com.myrecipestockpile.demo.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsersRepository extends CrudRepository<User, Long> {
+    
+}
+
