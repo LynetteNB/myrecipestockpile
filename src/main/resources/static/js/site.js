5a73a77bd61db67cd9e65ae2074f944e0c98ab6d
stephencisler <stephencisler@gmail.com>
2018-02-04 18:23:38 -0600

    ingredient fields are generating on click

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -8,7 +8,16 @@ $('a.like-button').on('click', function() {
 // Recipe Creation JS
 
 
-
+$('#addIngredient').click(function () {
+    $('#ingredientSec').append("<div class=\"col-sm-2\">" +
+        "<button type=\"button\" class=\"btn btn-outline-primary\" id=\"removeIng1\">Remove</button>" +
+        "<div class=\"col-sm-2\">" +
+        "<input class=\"form-control\" type=\"text\" name=\"quantity\"/>" +
+        "</div>" +
+        "<div class=\"col-sm-8\">" +
+        "<input class=\"form-control\" type=\"text\" name=\"ingredients\"/>" +
+        "</div>");
+});
 
 
 
