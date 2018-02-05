547288384717be844092485dea038258a3c7255c
stephencisler <stephencisler@gmail.com>
2018-02-04 19:22:13 -0600

    ingredient fields are generate and popped off with buttons

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -1,39 +1,37 @@
 $(document).ready(function () {
-    
-$('a.like-button').on('click', function() {
-    $(this).toggleClass('liked');
-});
+
+    $('a.like-button').on('click', function () {
+        $(this).toggleClass('liked');
+    });
 
 
 // Recipe Creation JS
 
 
-$('#addIngredient').click(function () {
-    $('#ingredientSec').append("<div class=\"col-sm-2\">" +
-        "<button type=\"button\" class=\"btn btn-outline-primary\" id=\"removeIng1\">Remove</button>" +
-        "<div class=\"col-sm-2\">" +
-        "<input class=\"form-control\" type=\"text\" name=\"quantity\"/>" +
-        "</div>" +
-        "<div class=\"col-sm-8\">" +
-        "<input class=\"form-control\" type=\"text\" name=\"ingredients\"/>" +
-        "</div>");
+    // Function to add on additional Quantity/Ingredient fields
+    $('#addIngredient').click(function () {
+        addIngredient();
+    });
+
+    $('#removeIngredient').click(function () {
+        console.log("remove");
+       $('.ingredientItem').filter(':last').remove();
+    });
+
+
+    function addIngredient() {
+        var html = "<div class=\"col-sm-12 ingredientItem\">\n" +
+            "<div class=\"row\">\n" +
+            "<div class=\"col-sm-2\">\n" +
+            "<input class=\"form-control\" type=\"text\" name=\"quantity\"/>\n" +
+            "</div>\n" +
+            "<div class=\"col-sm-8\">\n" +
+            "<input class=\"form-control\" type=\"text\" name=\"ingredients\"/>\n" +
+            "</div>\n" +
+            "</div>\n" +
+            "</div>"
+        $('#ingredientList').append(html);
+    }
+
+
 });
-
-
-
-
-
-
-
-
-
-
-
-
-});
-
-
-
-
-
-
