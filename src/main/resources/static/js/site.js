27973b5424095b1819f9b883abaf86089a519750
stephencisler <stephencisler@gmail.com>
2018-02-05 10:07:05 -0600

    can remove singles from list, but bug makes duplicate Ids sometimes

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -10,29 +10,49 @@ $(document).ready(function () {
     // Function to add on additional Quantity/Ingredient fields
     $('#addIngredient').click(function () {
         addIngredient();
+        addRemoveListeners();
     });
 
-    $('#removeIngredient').click(function () {
-        hideIng();
-        setTimeout(function () {
-            $('.ingredientItem').filter(':last').remove();
-        }, 600);
-    });
+    // $('#removeIngredient').click(function () {
+    //     hideIng();
+    //     setTimeout(function () {
+    //         $('.ingredientItem').filter(':last').remove();
+    //     }, 600);
+    // });
+
+    function addRemoveListeners() {
+        for (var i = 1; i <= $('.ingredientItem').length; i++) {
+            // var string = "#removeIng"
+            $('#removeIng' + i).off('click');
+            $('#removeIng' + i).click(function () {
+                var id = $(this).attr('id');
+                var index = id.substring(id.length - 1);
+                $('#ingId' + index).hide('slow');
+                setTimeout(function () {
+                    $('#ingId' + index).remove();
+                }, 600);
+            })
+        }
+    }
+
+
     
-    function removeIng() {
-        $('.ingredientItem').filter(':last').remove();
-    }
-
-    function hideIng() {
-       $('.ingredientItem').filter(':last').hide('slow');
-    }
-
+    // function removeIng() {
+    //     $('.ingredientItem').filter(':last').remove();
+    // }
+    //
+    //
+    //
+    // function hideIng() {
+    //    $('.ingredientItem').filter(':last').hide('slow');
+    // }
+    //
 
     function addIngredient() {
-        var item = $('.ingredientItem').length + 1;
-        var html = "<div class=\"col-sm-12 ingredientItem\" style=\"display: none\">\n" +
+        var itemNum = $('.ingredientItem').length + 1;
+        var html = "<div class=\"col-sm-12 ingredientItem\" style=\"display: none\" id=\"ingId" + itemNum + "\">" +
             "<div class=\"row\">\n" +
-            "<div class=\"col-sm-2\"><p>" + item + ")</p></div>" +
+            "<div class=\"col-sm-2\"><button type=\"button\" class=\"btn btn-outline-success btn-block\" id=\"removeIng" + itemNum + "\">Remove</button></div>" +
             "<div class=\"col-sm-2\">\n" +
             "<input class=\"form-control\" type=\"text\" name=\"quantity\"/>\n" +
             "</div>\n" +
@@ -42,7 +62,7 @@ $(document).ready(function () {
             "</div>\n" +
             "</div>"
         $('#ingredientList').append(html);
-        $('.ingredientItem').filter(':last').toggle('slow');
+        $('.ingredientItem').filter(':last').show('slow');
     }
 
 
