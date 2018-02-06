aab5b6ef89f6e659999373754be50e493aadce21
abbyperez <abbyperez28@gmail.com>
2018-02-06 10:00:04 -0600

    html for filestack removed while merging.

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -113,39 +113,16 @@ $(document).ready(function () {
         }
     });
 
-$(document).ready(function () {
 
-
-    $('a.like-button').on('click', function () {
-        $(this).toggleClass('liked');
-    });
-
-
-    const link = document.getElementById('url');
-    $('#uploadpicture').click(function () {
-        const client = filestack.init(AZvuYvrkS2GRaOBsg61tvz);
-        client.pick({}).then(function (result) {
-            const fileUrl = result.filesUploaded[0].url;
-            link.href = fileUrl;
-            link.innerHTML = fileUrl;
-            console.log(fileUrl);
-        });
-    });
-
-
-
-});
-
-
-// const link = document.getElementById('url');
-//
-// const client = filestack.init(AZvuYvrkS2GRaOBsg61tvz);
-// client.pick({
-//
-// }).then(function(result) {
-//     const fileUrl = result.filesUploaded[0].url;
-//     link.href=fileUrl;
-//     link.innerHTML=fileUrl;
-//     console.log(fileUrl);
-// });
+    // const link = document.getElementById('url');
+    // $('#uploadpicture').click(function () {
+    //     const client = filestack.init(AZvuYvrkS2GRaOBsg61tvz);
+    //     client.pick({}).then(function (result) {
+    //         const fileUrl = result.filesUploaded[0].url;
+    //         link.href = fileUrl;
+    //         link.innerHTML = fileUrl;
+    //         console.log(fileUrl);
+    //     });
+    // });
+}
 
